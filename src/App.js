a02f196b6d63bc6966579a8b571176fd611f7b14
Atharv Phadnis <athiphadnis@gmail.com>
2021-09-10 19:06:11 +0530

    Fully implemented Connect 4 game

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap'
 import './App.css';
 import Row from './Row';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function App() {
 	const [ board, setBoard ] = useState(Array(42).fill(""))
-	const [ turn, setTurn ] = useState("blue")
+	const [ turn, setTurn ] = useState("Blue")
+	const [ resultModal, setResultModal ] = useState(null)
+	useEffect(() => {
+		setResultModal(new bootstrap.Modal(document.getElementById('resultModal'), { keyboard: false, backdrop: 'static' }))
+	}, [])
+	const restart = () => window.location.reload()
 	const handleClick = (e) => {
 		const tile = e.target.querySelector('input').value
 		const position = getLowestPlace(tile)
 		let _board = [ ...board ]
 		if(position !== false) {
-			_board[position] = turn === "blue" ? "blue" : "red"
+			_board[position] = turn === "Blue" ? "Blue" : "Red"
 			setBoard(_board)
-			setTurn(_turn => turn === "blue" ? "red" : "blue")
+			if(checkWin(position)) {
+				resultModal.show()
+			}
+			else {
+				setTurn(_turn => turn === "Blue" ? "Red" : "Blue")
+			}
 		}
 	}
 	const getLowestPlace = (current) => {
@@ -27,11 +38,74 @@ export default function App() {
 		}
 		return false
 	}
-	
+	const checkWin = (current) => {
+		const column = current % 7
+		const row = (current - column) / 7
+		let nw = Math.min(column, row)
+		let ne = Math.min(6-column, row)
+		let count = 0
+		// Horizontal
+		for(let temp_column = 0; temp_column < 7; ++temp_column) {
+			if(board[row*7 + temp_column] === turn) {
+				if(++count > 2) return true
+			}
+			else {
+				count = 0
+			}
+		}
+		count = 0
+		// Vertical
+		for(let temp_row = 0; temp_row < 6; ++temp_row) {
+			if(board[temp_row*7 + column] === turn) {
+				if(++count > 2) return true
+			}
+			else {
+				count = 0
+			}
+		}
+		count = 0
+		// L2R Diagonal
+		for(let temp_row = row - nw, temp_column = column - nw; temp_row < 5 || temp_column < 6; ++temp_row, ++temp_column) {
+			if(board[temp_row*7 + temp_column] === turn) {
+				if(++count > 2) return true
+			}
+			else {
+				count = 0
+			}
+		}
+		count = 0
+		// R2l Diagonal
+		for(let temp_row = row - ne, temp_column = column + ne; temp_row < 5 || temp_column > 0; ++temp_row, --temp_column) {
+			if(board[temp_row*7 + temp_column] === turn) {
+				if(++count > 2) return true
+			}
+			else {
+				count = 0
+			}
+		}
+		count = 0
+		return false
+	}
 	return(
 		<main>
+			<div className="modal fade" id="resultModal" tabIndex="-1" aria-labelledby="resultModalLabel" aria-hidden="true">
+				<div className="modal-dialog modal-dialog-centered">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h3 className="modal-title" id="resultModalLabel">Game Over</h3>
+						</div>
+						<div className="modal-body">
+							<h5><span className={turn === "Blue" ? "text-primary" : "text-danger"}>{turn}</span> has won the game!</h5>
+						</div>
+						<div className="modal-footer">
+							<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+							<button type="button" className="btn btn-primary" onClick={restart}>Restart</button>
+						</div>
+					</div>
+				</div>
+			</div>
 			<div className="card shadow-lg text-center p-3">
-				<h1 className={turn ? "text-primary" : "text-danger"}>Connect 4</h1>
+				<h1 className={turn === "Blue" ? "text-primary" : "text-danger"}>Connect 4</h1>
 				<table className="table bg-secondary table-bordered m-0">
 					<thead></thead>
 					<tbody>
